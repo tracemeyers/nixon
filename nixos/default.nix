a67ff27c6fe44791ae6f4422f46cdafe766f8807
Trace Meyers <1397338+tracemeyers@users.noreply.github.com>
2023-08-27 20:29:12 -0500

    wip

--- nixos/default.nix
+++ nixos/default.nix
@@ -135,7 +135,7 @@
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
-      inputs.agenix.overlays.default
+      #inputs.agenix.overlays.default
 
       # Or define it inline, for example:
       # (final: prev: {
