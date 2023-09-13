b33f9aadc07b6cbd245fd9642aca1f68a1d25320
Trace Meyers <1397338+tracemeyers@users.noreply.github.com>
2023-09-12 20:51:42 -0500

    more

--- nixos/_mixins/desktop/plasma-i3.nix
+++ nixos/_mixins/desktop/plasma-i3.nix
@@ -79,13 +79,8 @@
             # kill focused window
             bindsym $mod+Shift+q kill
             
-            # start dmenu (a program launcher)
-            #bindsym $mod+d exec dmenu_run
-            bindsym $mod+d exec rofi -show run
-            # There also is the (new) i3-dmenu-desktop which only displays applications
-            # shipping a .desktop file. It is a wrapper around dmenu, so you need that
-            # installed.
-            # bindsym $mod+d exec --no-startup-id i3-dmenu-desktop
+            # krunner launcher
+            bindsym $mod+d exec --no-startup-id /run/current-system/sw/bin/qdbus org.kde.krunner /App display
             
             # change focus
             bindsym $mod+h focus left
@@ -131,12 +126,6 @@
             # change focus between tiling / floating windows
             bindsym $mod+space focus mode_toggle
             
-            # focus the parent container
-            bindsym $mod+a focus parent
-            
-            # focus the child container
-            #bindsym $mod+d focus child
-            
             # switch to workspace
             bindsym $mod+1 workspace 1
             bindsym $mod+2 workspace 2
@@ -165,51 +154,39 @@
             bindsym $mod+Shift+c reload
             # restart i3 inplace (preserves your layout/session, can be used to upgrade i3)
             bindsym $mod+Shift+r restart
-            # exit i3 (logs you out of your X session)
-            #bindsym $mod+Shift+e exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'"
             
             # resize window (you can also use the mouse for that)
             mode "resize" {
-                    # These bindings trigger as soon as you enter the resize mode
+              # These bindings trigger as soon as you enter the resize mode
             
-                    # Pressing left will shrink the window’s width.
-                    # Pressing right will grow the window’s width.
-                    # Pressing up will shrink the window’s height.
-                    # Pressing down will grow the window’s height.
-                    bindsym h resize grow width 10 px or 10 ppt
-                    bindsym k resize grow height 10 px or 10 ppt
-                    bindsym j resize shrink height 10 px or 10 ppt
-                    bindsym l resize shrink width 10 px or 10 ppt
+              # Pressing left will shrink the window’s width.
+              # Pressing right will grow the window’s width.
+              # Pressing up will shrink the window’s height.
+              # Pressing down will grow the window’s height.
+              bindsym h resize grow width 10 px or 10 ppt
+              bindsym k resize grow height 10 px or 10 ppt
+              bindsym j resize shrink height 10 px or 10 ppt
+              bindsym l resize shrink width 10 px or 10 ppt
             
-                    # same bindings, but for the arrow keys
-                    bindsym Left resize shrink width 10 px or 10 ppt
-                    bindsym Down resize grow height 10 px or 10 ppt
-                    bindsym Up resize shrink height 10 px or 10 ppt
-                    bindsym Right resize grow width 10 px or 10 ppt
+              # same bindings, but for the arrow keys
+              bindsym Left resize shrink width 10 px or 10 ppt
+              bindsym Down resize grow height 10 px or 10 ppt
+              bindsym Up resize shrink height 10 px or 10 ppt
+              bindsym Right resize grow width 10 px or 10 ppt
             
-                    # back to normal: Enter or Escape
-                    bindsym Return mode "default"
-                    bindsym Escape mode "default"
+              # back to normal: Enter or Escape
+              bindsym Return mode "default"
+              bindsym Escape mode "default"
             }
             bindsym $mod+r mode "resize"
             
             bindsym $mod+shift+s sticky toggle
             
-            ## Start i3bar to display a workspace bar (plus the system information i3status
-            ## finds out, if available)
-            #bar {
-            ##        status_command i3status
-            #    status_command SCRIPT_DIR=~/.config/i3blocks i3blocks
-            #    #font pango:DejaVu Sans Mono 14
-            #    font pango:monospace 12
-            #}
-            
-            #exec /usr/bin/vmware-user
-            #
-            exec --no-startup-id deadd-notification-center
-            #exec --no-startup-id compton --config ~/.config/compton.conf -b
-            
             bindsym $mod+n exec ~/bin/notifications.sh
+
+            bar {
+                mode dock
+            }
             
             for_window [class="Slack" floating] move container to workspace current
             
@@ -234,12 +211,7 @@
             for_window [title="Desktop — Plasma"] kill; floating enable; border none
             # using plasma's logout screen instead of i3's
             bindsym $mod+Shift+e exec --no-startup-id qdbus org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.logout -1 -1 -1
-            # Kill the bar
-            bar {
-                mode hide
-            }
 
-            for_window [title="Desktop — Plasma"] kill, floating enable, border none
             for_window [class="plasmashell"] floating enable
             for_window [class="Plasma"] floating enable, border none
             for_window [title="plasma-desktop"] floating enable, border none
